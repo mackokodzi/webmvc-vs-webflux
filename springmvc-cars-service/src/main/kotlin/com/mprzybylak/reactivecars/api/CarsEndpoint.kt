6b4073f942fe1c2e278a52d7a14c76c046d03b92
Maciej Przybylak <maciej.przybylak@allegro.pl>
2019-09-04 22:23:17 +0200

    Spring MVC Cars Service

---
+++ springmvc-cars-service/src/main/kotlin/com/mprzybylak/reactivecars/api/CarsEndpoint.kt
@@ -0,0 +1,24 @@
+package com.mprzybylak.reactivecars.api
+
+import com.mprzybylak.reactivecars.utils.logger
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+
+@RestController
+class CarsEndpoint {
+
+    @PostMapping("/nomongo/delay/cars")
+    fun insertCarWithDelayWithoutMongo(): CarApi {
+        logger.info("Inserting car with delay without mongo")
+        Thread.sleep(DELAY)
+        return CarApi(UUID.randomUUID().toString())
+    }
+
+    companion object {
+        val logger by logger()
+        const val DELAY : Long = 100
+    }
+}
+
+data class CarApi(val id: String)
